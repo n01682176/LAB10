787a04c0c3e7ac1906d73e13efc5835cef518c29
n01682176 <aapeksha080@gmail.com>
2024-07-24 17:00:09 -0400

    main

---
+++ script.js
@@ -0,0 +1,14 @@
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+$(document).ready(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+  });
+});
